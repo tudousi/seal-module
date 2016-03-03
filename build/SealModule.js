6736e09b0f1664ce089e9e781b301172611267a9
蛋蛋 <41871879@qq.com>
2016-03-04 01:46:01 +0800

    完成充血模型

--- build/SealModule.js
+++ build/SealModule.js
@@ -7,6 +7,7 @@
     root.SealModule = factory(root.jQuery);
   }
 }(this, function($) {
+SealModule.prototype.opts = {}
 function SealModule(){}
 // 给类扩展静态属性和方法
 SealModule.extend = function(obj) {
@@ -21,6 +22,63 @@ SealModule.extend = function(obj) {
     }
     return obj.extended ? obj.extended.call(this) : null;
 };
+// 给类扩展原型方法
+SealModule.include = function(obj) {
+    var key;
+    if(obj == null || (typeof obj !== 'object')) {
+        return;
+    }
+    for(key in obj) {
+        if(key !== 'extended' && key !== 'included') {
+            this.prototype[key] = obj[key];
+        }
+    }
+    return obj.included ? obj.included.call(this) : null;
+};
+// 给类链接插件或者扩展功能
+SealModule.connect = function(cls) {
+    if(typeof cls !== 'function') {
+        return;
+    }
+    if(!cls.pluginName) {
+        throw new Error("SealModule.connect: cannot connect plugin without pluginName.");
+        return;
+    }
+    cls.prototype._connected = true;
+    if(!this._connectedClasses) {
+        this._connectedClasses = [];
+    }
+    this._connectedClasses.push(cls);
+    return this[cls.pluginName] = cls;
+}
+SealModule.prototype._init = function() {
+
+};
+SealModule.prototype.on = function() {
+
+};
+SealModule.prototype.one = function() {
+
+};
+SealModule.prototype.off = function() {
+
+};
+SealModule.prototype.trigger = function() {
+
+};
+SealModule.prototype.triggerHandler = function() {
+
+};
+SealModule.prototype._t = function() {
+
+};
+SealModule._t = function() {
+
+};
+SealModule.i18n = function() {
+
+};
+SealModule.locale = "zh-CN";
 
 return SealModule;
 }));
--- src/SealModule.js
+++ src/SealModule.js
@@ -1,4 +1,28 @@
-function SealModule(){}
+SealModule.prototype.opts = {}
+function SealModule(opts){
+    var instances;
+    this.opts = $.extend({}, this.opts, opts);
+    if(!this.constructor._connectedCLasses) {
+        this.constructor._connectedCLasses = [];
+    }
+    instances = (function(){
+        var i;
+        var len;
+        var cls;
+        var name;
+        var results = [];
+        var connected = this.constructor._connectedClasses;
+        for(i = 0, len = connected.length; i < len; i++) {
+            cls = connected[i];
+            name = cls.pluginName.charAt(0).toLowerCase() + cls.pluginName.slice(1);    // 插件名称转首字符换为小写
+            if(cls.prototype._connected) {  // 如果插件已经被实例化则当前插件引用this
+                cls.prototype._module = this;
+            }
+            results.push(this[name] = new cls());
+        }
+        return results;
+    }).call(this);
+}
 // 给类扩展静态属性和方法
 SealModule.extend = function(obj) {
     var key;
@@ -12,3 +36,58 @@ SealModule.extend = function(obj) {
     }
     return obj.extended ? obj.extended.call(this) : null;
 };
+SealModule.include = function(obj) {
+    var key;
+    if(obj == null || (typeof obj !== 'object')) {
+        return;
+    }
+    for(key in obj) {
+        if(key !== 'extended' && key !== 'included') {
+            this.prototype[key] = obj[key];
+        }
+    }
+    return obj.included ? obj.included.call(this) : null;
+};
+SealModule.connect = function(cls) {
+    if(typeof cls !== 'function') {
+        return;
+    }
+    if(!cls.pluginName) {
+        throw new Error("SealModule.connect: cannot connect plugin without pluginName.");
+        return;
+    }
+    cls.prototype._connected = true;
+    if(!this._connectedClasses) {
+        this._connectedClasses = [];
+    }
+    this._connectedClasses.push(cls);
+    return this[cls.pluginName] = cls;
+}
+SealModule.prototype._init = function() {
+
+};
+SealModule.prototype.on = function() {
+
+};
+SealModule.prototype.one = function() {
+
+};
+SealModule.prototype.off = function() {
+
+};
+SealModule.prototype.trigger = function() {
+
+};
+SealModule.prototype.triggerHandler = function() {
+
+};
+SealModule.prototype._t = function() {
+
+};
+SealModule._t = function() {
+
+};
+SealModule.i18n = function() {
+
+};
+SealModule.locale = "zh-CN";
